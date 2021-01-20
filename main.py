4eafc9877710f298b96c557f7020eb33e4c29a61
Naveen Krishna <sansnk882000@gmail.com>
2021-01-20 13:13:00 +0530

    Now, the students can place their order. Only orders from the same hotel will be accepted. Cart, total amount and the number of items are added.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from kivymd.uix.snackbar import Snackbar
 from kivymd.uix.toolbar import MDToolbar
 from kivymd.uix.list import TwoLineAvatarIconListItem, IconLeftWidget, IconRightWidget
 from kivymd.toast import toast
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton
 
 Window.size = (360, 600)
 Window.keyboard_anim_args = {'d': 0.2, 't': 'in_out_expo'}
@@ -64,6 +66,9 @@ class TopToolbar(MDToolbar):
 class HotelScreen(Screen):
     global db_cafe
     dish_ordering_list = list()
+    dishCount_Order = 0
+    totalAmount_Order = 0
+    cafe_ordering_currently = ""
 
     def on_enter(self):
         toast(text="Welcome!")
@@ -79,35 +84,65 @@ class HotelScreen(Screen):
         self.ids.list_dish.clear_widgets()
         search = "select dish, price from dish where username = \"" + hotel_selected + "\" and available = 1"
         self.cursor.execute(search)
-        dish_available = pd.DataFrame(self.cursor.fetchall()).rename(columns={0: "Dish", 1: "Price"})
-        for i in range(len(dish_available)):
+        self.dish_available = pd.DataFrame(self.cursor.fetchall()).rename(columns={0: "Dish", 1: "Price"})
+        for i in range(len(self.dish_available)):
             l = IconLeftWidget(icon="minus", on_release=self.minus_dish)
             r = IconRightWidget(icon="plus", on_release=self.plus_dish)
-            dish_toShow = TwoLineAvatarIconListItem(text=dish_available.loc[i, "Dish"],
-                                                    secondary_text=str(dish_available.loc[i, "Price"]))
+            dish_toShow = TwoLineAvatarIconListItem(text=self.dish_available.loc[i, "Dish"],
+                                                    secondary_text=str(self.dish_available.loc[i, "Price"]))
             dish_toShow.add_widget(l)
             dish_toShow.add_widget(r)
             self.ids.list_dish.add_widget(dish_toShow)
 
     def minus_dish(self, instance):
         dish_clicked_toMinus = [widget for widget in instance.walk_reverse()][5]
-        # print(dish_clicked_toMinus.text)
         try:
             self.dish_ordering_list.remove(dish_clicked_toMinus.text)
-            Snackbar(text=dish_clicked_toMinus.text + " has been removed from cart").show()
+            # Snackbar(text=dish_clicked_toMinus.text + " has been removed from cart").show()
+            # print(dish_clicked_toMinus.text,int(self.dish_available.query('Dish == @dish_clicked_toMinus.text')['Price']))
+            self.dishCount_Order -= 1
+            self.totalAmount_Order -= int(self.dish_available.query('Dish == @dish_clicked_toMinus.text')['Price'])
+            # print(self.dishCount_Order, self.totalAmount_Order)
+            self.ids.bottomToolbar.title = str(self.dishCount_Order) + " items | \u20B9" + str(self.totalAmount_Order)
+
         except ValueError:
             pass
 
     def plus_dish(self, instance):
-        dish_clicked_toPlus = [widget for widget in instance.walk_reverse()][8]
-        # print(dish_clicked_toPlus.text)
-        self.dish_ordering_list.append(dish_clicked_toPlus.text)
-        Snackbar(text=dish_clicked_toPlus.text + " has been added to cart").show()
-        # dish_clicked = [widget for widget in instance.walk_reverse()][8]
-        # print(dish_clicked)
-        # print(dish_clicked.text)
-        # print([type(widget) for widget in instance.walk_reverse()])
-        # print(dir(instance))
+        self.cafe_ordering_currently = self.ids.hotelNames_spinner.text if self.cafe_ordering_currently == "" else self.cafe_ordering_currently
+        # print(self.cafe_ordering_currently, self.ids.hotelNames_spinner.text)
+        if self.cafe_ordering_currently == self.ids.hotelNames_spinner.text:
+            dish_clicked_toPlus = [widget for widget in instance.walk_reverse()][8]
+            self.dish_ordering_list.append(dish_clicked_toPlus.text)
+            # Snackbar(text=dish_clicked_toPlus.text + " has been added to cart").show()
+            # print(dish_clicked_toPlus.text, int(self.dish_available.query('Dish == @dish_clicked_toPlus.text')['Price']))
+            self.dishCount_Order += 1
+            self.totalAmount_Order += int(self.dish_available.query('Dish == @dish_clicked_toPlus.text')['Price'])
+            # print(self.dishCount_Order, self.totalAmount_Order)
+            self.ids.bottomToolbar.title = str(self.dishCount_Order) + " items | \u20B9" + str(self.totalAmount_Order)
+
+        else:
+            self.dialog = MDDialog(
+                title="Replace cart item?",
+                text="Your cart has items from " + self.cafe_ordering_currently + ". Do you wish to replace it?",
+                buttons=[
+                    MDFlatButton(text="NO", on_release=self.dialogResponse),
+                    MDFlatButton(text="YES", on_release=self.dialogResponse),
+                ],
+                size_hint_x = 0.75
+            )
+            self.dialog.open()
+
+    def dialogResponse(self, responseVal):
+        if responseVal.text == "YES":
+            self.dish_ordering_list = list()
+            self.dishCount_Order = 0
+            self.totalAmount_Order = 0
+            self.cafe_ordering_currently = ""
+            self.ids.bottomToolbar.title = ""
+            self.dialog.dismiss()
+        else:
+            self.dialog.dismiss()
 
     def verifyOrders_bottom(self):
         orders__frequency_table = {i: self.dish_ordering_list.count(i) for i in self.dish_ordering_list}
