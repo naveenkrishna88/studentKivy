1e83aa5d2c13d766bc1a8d5209acbeead5ead5db
naveenkrishna88 <41195593+naveenkrishna88@users.noreply.github.com>
2021-03-05 13:40:00 +0530

    Update main.py
    Updated  the database credentials

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ from kivymd.uix.navigationdrawer import MDNavigationDrawer
 from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelTwoLine
 
 
-# Window.size = (360, 600)
+Window.size = (360, 600)
 Window.keyboard_anim_args = {'d': 0.2, 't': 'in_out_expo'}
 Window.softinput_mode = 'below_target'
 
-db = mysql.connector.connect(host="bpjum1fu8uithbn7fk2n-mysql.services.clever-cloud.com", user="utt8pcxyfysh9vwz",
-                                  passwd="fFYduFGCjwhnyLrc1hIy", database="bpjum1fu8uithbn7fk2n", autocommit = True)
+db = mysql.connector.connect(host="btyjrjkqioozicbjpsyz-mysql.services.clever-cloud.com", user="utt8pcxyfysh9vwz",passwd="fFYduFGCjwhnyLrc1hIy", database="btyjrjkqioozicbjpsyz", autocommit = True)
 
 username_current = ""
 
@@ -136,21 +135,15 @@ class HotelScreen(Screen):
     def on_pre_enter(self):
         db.cmd_reset_connection()
         HotelScreen.bottomAppID = self.ids.bottomToolbar
-        # print(HotelScreen.bottomAppID)
         self.cursor = db.cursor()
         search = "select distinct username from dish"
         self.cursor.execute(search)
         self.hotel_names_spinner = list(pd.DataFrame(self.cursor.fetchall())[0])
         self.ids.hotelNames_spinner.values = self.hotel_names_spinner
-        # self.ids.bottomToolbar.title = ""
         self.cursor.close()
 
     def dish_filler(self, hotel_selected):
-        # print(dir(self.ids.list_dish))
-        # print("Children before :\t", self.ids.list_dish.children)
         self.ids.list_dish.clear_widgets()
-        # print("Children after :\t", self.ids.list_dish.children)
-        # print(dir(self.ids.list_dish))
         self.cursor = db.cursor()
         search = "select dish, price from dish where username = \"" + hotel_selected + "\" and available = 1"
         self.cursor.execute(search)
@@ -160,7 +153,7 @@ class HotelScreen(Screen):
             l = IconLeftWidget(icon="minus", on_release=self.minus_dish)
             r = IconRightWidget(icon="plus", on_release=self.plus_dish)
             dish_toShow = TwoLineAvatarIconListItem(text=HotelScreen.dish_available_in_currentSelectedHotel.loc[i, "Dish"],
-                                                    secondary_text=str(HotelScreen.dish_available_in_currentSelectedHotel.loc[i, "Price"]))
+                                                    secondary_text=str('\u20B9') + str(HotelScreen.dish_available_in_currentSelectedHotel.loc[i, "Price"]))
             dish_toShow.add_widget(l)
             dish_toShow.add_widget(r)
             self.ids.list_dish.add_widget(dish_toShow)
@@ -169,11 +162,8 @@ class HotelScreen(Screen):
         dish_clicked_toMinus = [widget for widget in instance.walk_reverse()][5]
         try:
             HotelScreen.student_ordering_list.remove(dish_clicked_toMinus.text)
-            # Snackbar(text=dish_clicked_toMinus.text + " has been removed from cart").show()
-            # print(dish_clicked_toMinus.text,int(HotelScreen.dish_available_in_currentSelectedHotel.query('Dish == @dish_clicked_toMinus.text')['Price']))
             HotelScreen.dishCount_Order -= 1
             HotelScreen.totalAmount_Order -= int(HotelScreen.dish_available_in_currentSelectedHotel.query('Dish == @dish_clicked_toMinus.text')['Price'])
-            # print(HotelScreen.dishCount_Order, HotelScreen.totalAmount_Order)
             self.ids.bottomToolbar.title = str(HotelScreen.dishCount_Order) + " items | \u20B9" + str(HotelScreen.totalAmount_Order)
 
         except ValueError:
@@ -181,15 +171,11 @@ class HotelScreen(Screen):
 
     def plus_dish(self, instance):
         HotelScreen.hotel_ordering_currently = self.ids.hotelNames_spinner.text if HotelScreen.hotel_ordering_currently == "" else HotelScreen.hotel_ordering_currently
-        # print(HotelScreen.hotel_ordering_currently, self.ids.hotelNames_spinner.text)
         if HotelScreen.hotel_ordering_currently == self.ids.hotelNames_spinner.text:
             dish_clicked_toPlus = [widget for widget in instance.walk_reverse()][8]
             HotelScreen.student_ordering_list.append(dish_clicked_toPlus.text)
-            # Snackbar(text=dish_clicked_toPlus.text + " has been added to cart").show()
-            # print(dish_clicked_toPlus.text, int(HotelScreen.dish_available_in_currentSelectedHotel.query('Dish == @dish_clicked_toPlus.text')['Price']))
             HotelScreen.dishCount_Order += 1
             HotelScreen.totalAmount_Order += int(HotelScreen.dish_available_in_currentSelectedHotel.query('Dish == @dish_clicked_toPlus.text')['Price'])
-            # print(HotelScreen.dishCount_Order, HotelScreen.totalAmount_Order)
             self.ids.bottomToolbar.title = str(HotelScreen.dishCount_Order) + " items | \u20B9" + str(HotelScreen.totalAmount_Order)
 
         else:
@@ -225,10 +211,6 @@ class HotelScreen(Screen):
         HotelScreen.totalAmount_Order = 0
         HotelScreen.hotel_ordering_currently = ""
         self.dish_available_in_currentSelectedHotel = pd.DataFrame()
-        # print(self.ids.hotelNames_spinner.text, self.ids.bottomToolbar.title, self.ids.bottomToolbar)
-        # print(MDApp.get_running_app())
-        # print(dir(MDApp.get_running_app()))
-        # print("reset: ", HotelScreen.bottomAppID)
         HotelScreen.bottomAppID.title = ""
 
 
@@ -239,38 +221,22 @@ class BillScreen(Screen):
     def on_enter(self):
         db.cmd_reset_connection()
         self.hotel_obj = HotelScreen()
-        # self.time_takeAway = None
         self.orders_frequency_table = None
         self.cursor = db.cursor()
         self.ids.bill_hotelName.text = str(self.hotel_obj.hotel_ordering_currently).upper()
-        # print(self.hotel_obj.hotel_ordering_currently, self.hotel_obj.student_ordering_list, self.hotel_obj.dish_available_in_currentSelectedHotel)
         self.orders_frequency_table = {i: self.hotel_obj.student_ordering_list.count(i) for i in self.hotel_obj.student_ordering_list}
-        # print(self.orders_frequency_table)
         self.ids.toBill_ScrollView.clear_widgets()
         for i in self.orders_frequency_table:
             grid_for_bill_card = MDGridLayout(cols=2, adaptive_height = True)
-            # print(i, self.orders_frequency_table[i])
-            # print(self.hotel_obj.dish_available_in_currentSelectedHotel)
-            # print(int(self.hotel_obj.dish_available_in_currentSelectedHotel.query('Dish == @i')['Price']))
-            # dish_price_toOrder = TwoLineListItem(text=i,secondary_text=str(HotelScreen.dish_available_in_currentSelectedHotel.loc[i, "Price"]))
-            # dish_quantity_toOrder = MDLabel(text = str(int(self.hotel_obj.dish_available_in_currentSelectedHotel.query('Dish == @i')['Price'])))
             billDishName_details = TwoLineListItem(text = str(i), secondary_text = "QTY - " + str(self.orders_frequency_table[i]))
             billDishPrice_details = MDLabel(text = str(int(self.hotel_obj.dish_available_in_currentSelectedHotel.query('Dish == @i')['Price']) * self.orders_frequency_table[i]), halign = 'center')
-            # self.ids.billDishName_details_id.text = str(i)
-            # self.ids.billDishName_details_id.secondary_text = "QTY - " + str(self.orders_frequency_table[i])
-            # self.ids.billDishPrice_details_id.text = str(int(self.hotel_obj.dish_available_in_currentSelectedHotel.query('Dish == @i')['Price']) * self.orders_frequency_table[i])
-            # self.ids.billDishPrice_scrollView.add_widget(billDishPrice_details)
-            # self.ids.billDishPrice_scrollView.add_widget(billDishPrice_details)
             grid_for_bill_card.add_widget(billDishName_details)
             grid_for_bill_card.add_widget(billDishPrice_details)
             self.ids.toBill_ScrollView.add_widget(grid_for_bill_card)
         self.ids.billTotalAmountToPay_id.text = "\u20B9 " + str(self.hotel_obj.totalAmount_Order)
-        # print("text = ", self.ids.billTotalAmountToPay_id.text)
-        # print(self.hotel_obj.totalAmount_Order)
         self.cursor.close()
 
     def show_time_picker(self):
-        # previous_time = datetime.now().time()
         time_dialog = MDTimePicker()
         time_dialog.bind(time=self.get_timeFromPicker)
         time_dialog.set_time(datetime.now().time())
@@ -282,36 +248,24 @@ class BillScreen(Screen):
         self.ids.billConfirmBtn.disabled = False
 
     def placeOrder(self):
-        # print(username_current)
-        # print("1st line ", self.hotel_obj.hotel_ordering_currently)
-        # print(datetime.combine(date.today(), self.time_takeAway))
-        # print(self.orders_frequency_table)
-        orderNum = str(randint(0,99999)) + str(date.today().year) + str(date.today().month) + str(date.today().day)
-        # print(orderNum)
-        self.cursor = db.cursor()
-        placeOrderStatement = "insert into orderDetailsTimeTable(ordernum, studentUsername, hotelName, timeTakeAway, totalPay, orderStatus) values (%s, %s, %s, %s, %s, %s)"
-        placeOrderDetails = (orderNum, username_current, self.hotel_obj.hotel_ordering_currently, datetime.combine(date.today(), self.time_takeAway), self.hotel_obj.totalAmount_Order, True)
-        self.cursor.execute(placeOrderStatement, placeOrderDetails)
 
-        placeOrderDishTableDetails = []
-        for i in self.orders_frequency_table:
-            placeOrderDishTableDetails.extend([orderNum, i, self.orders_frequency_table[i]])
-        # print(self.orders_frequency_table)
-        # print(placeOrderDishTableDetails)
-
-        placeOrderDishTableStatement = "insert into orderDishTable(ordernum, dish, quantity) values " + ",".join("(%s, %s, %s)" for dummy in self.orders_frequency_table)
-        # print(placeOrderDishTableStatement)
-        # print("Out if", self.hotel_obj.hotel_ordering_currently)
         if datetime.combine(date.today(), self.time_takeAway) > datetime.now():
+
+            orderNum = str(randint(10000, 99999)) + str(date.today().year) + str(date.today().month) + str(date.today().day)
+            self.cursor = db.cursor()
+            placeOrderStatement = "insert into orderDetailsTimeTable(ordernum, studentUsername, hotelName, timeTakeAway, totalPay, orderStatus) values (%s, %s, %s, %s, %s, %s)"
+            placeOrderDetails = (orderNum, username_current, self.hotel_obj.hotel_ordering_currently,datetime.combine(date.today(), self.time_takeAway), self.hotel_obj.totalAmount_Order,True)
+            self.cursor.execute(placeOrderStatement, placeOrderDetails)
+
+            placeOrderDishTableDetails = []
+            for i in self.orders_frequency_table:
+                placeOrderDishTableDetails.extend([orderNum, i, self.orders_frequency_table[i]])
+
+            placeOrderDishTableStatement = "insert into orderDishTable(ordernum, dish, quantity) values " + ",".join(
+                "(%s, %s, %s)" for dummy in self.orders_frequency_table)
+
             self.ids.bill_timeLbl.text = "00:00"
             self.ids.billConfirmBtn.disabled = True
-            # print("Out For",self.hotel_obj.hotel_ordering_currently)
-            for i in self.orders_frequency_table:
-                update_dishOutstanding = "update dish set orderOutStanding = orderOutStanding + %s where dish = %s and username = %s"
-                update_dishOutstanding_details = (str(self.orders_frequency_table[i]), str(i), str(self.hotel_obj.hotel_ordering_currently))
-                # print("in for", self.hotel_obj.hotel_ordering_currently)
-                # print(update_dishOutstanding_details)
-                self.cursor.execute(update_dishOutstanding, update_dishOutstanding_details)
             self.cursor.execute(placeOrderDishTableStatement, placeOrderDishTableDetails)
             db.commit()
             self.cursor.close()
